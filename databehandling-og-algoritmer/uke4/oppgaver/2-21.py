d756e3368fd1ffbc454819a86133ded72b653f63
HenrikS-A <hstromandersen@gmail.com>
2024-01-31 17:42:14 +0100

    rettet oppg 2-21

--- databehandling-og-algoritmer/uke4/oppgaver/2-21.py
+++ databehandling-og-algoritmer/uke4/oppgaver/2-21.py
@@ -18,12 +18,28 @@ for kategori in sortert_kategorier[:3]:
 
 for i, kategori in enumerate(topp_3_kategorier):
     sum_rating = 0
-    sum_antall_installasjoner = 0 # Fiks dette !!!!!!
+    sum_installasjoner = 0 # Fiks dette !!!!!!
     for app in appene:
         if app["Category"] == kategori["kategori"]:
             if app["Rating"] != "NaN":
                 sum_rating += float(app["Rating"])
+            
+            installasjoner = app["Installs"]
+            installasjoner = installasjoner.replace(",", "")
+            installasjoner = installasjoner.replace("+", "")
+            sum_installasjoner += int(installasjoner)
 
-    gjennomsnitt = sum_rating/kategori["antall_apper"]
-    topp_3_kategorier[i]["snittrating"] = gjennomsnitt
-    pass
+    rating_snitt = sum_rating/kategori["antall_apper"]
+    topp_3_kategorier[i]["snittrating"] = round(rating_snitt, 2)
+
+    installering_snitt = sum_installasjoner/kategori["antall_apper"]
+    topp_3_kategorier[i]["snitt_installasjoner"] = round(installering_snitt, 2)
+
+
+print(" -- De største kategoriene -- ")
+for i, kategori in enumerate(topp_3_kategorier):
+    print(f"{i+1}: {kategori['kategori']}")
+    print(f"\tAntall apper: {kategori['antall_apper']}")
+    print(f"\tSnittrating: {kategori['snittrating']}")
+    print(f"\tSnitt antall installasjoner: {kategori['snitt_installasjoner']}")
+
