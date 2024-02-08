264a20a0cd7469e0ff958d4618f13212e63ee3fd
HenrikS-A <hstromandersen@gmail.com>
2024-02-08 11:20:53 +0100

    prøven

---
+++ IT2-nettbok-guide.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/thorcc/IT2-nettbok
+cd IT2-nettbok  # gå inn i mappa IT2-nettbok
+pnpm install    # installerer alle pakker
+pnpm run dev    # kjør webserveren
+# Gå inn på nettadressen http://localhost:3000
