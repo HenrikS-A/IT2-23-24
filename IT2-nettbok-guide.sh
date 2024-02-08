59c78ea133186353ac3bd063180c12d1b8d12726
HenrikS-A <hstromandersen@gmail.com>
2024-02-08 20:04:38 +0100

    nrk case oppgave

--- IT2-nettbok-guide.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/thorcc/IT2-nettbok
-cd IT2-nettbok  # gå inn i mappa IT2-nettbok
-pnpm install    # installerer alle pakker
-pnpm run dev    # kjør webserveren
-# Gå inn på nettadressen http://localhost:3000
---
+++ databehandling-og-algoritmer/uke6-prove/nrk-case/test.py
@@ -0,0 +1,70 @@
+import requests
+import json
+
+def bestem_brukertype(bruker: str):
+    url = f"https://api.github.com/users/{bruker}"
+    
+    headers = {
+        "Accept": "",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    respons = requests.get(url, headers=headers, timeout=10)
+    data = respons.json()
+
+    return data["type"]
+
+def hent_repoer(bruker: str, brukertype):
+    if brukertype == "User":
+        typen = "users"
+    elif brukertype == "Organization":
+        typen = "orgs"
+    else:
+        print("FEIL!")
+
+    url = f"https://api.github.com/{typen}/{bruker}/repos"
+    
+    headers = {
+        "Accept": "",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    respons = requests.get(url, headers=headers, timeout=10)
+    data = respons.json()
+
+    return data
+
+bruker = input("Skriv inn et brukernavn eller organisasjonsnavn \n >")
+
+brukertype = bestem_brukertype(bruker) # Eks: "octocat" / "Microsoft"
+repoer = hent_repoer(bruker, brukertype)
+
+# --
+json_objekt = json.dumps(repoer, indent=4)
+with open("info.json", "w") as fil:
+    fil.write(json_objekt)
+# --
+    
+
+print(f"\nRepoene til {bruker}:")
+
+for repo in repoer:
+    utskrift_beskrivelser = ["Navn", "URL", "Beskrivelse", "Temaer", "Lisens navn", "Programmeringsspråk"]
+    utskrifter = [repo["name"], repo['html_url'], repo['description'], repo['topics'], repo["license"], repo["language"]]
+    
+    
+    utskrifter = [utskrift if utskrift != None and utskrift != [] else "-" for utskrift in utskrifter]
+    # Dette gjør det samme som one-lineren over:
+    # utskrifter_placeholder = []
+    # for utskrift in utskrifter:
+    #     if utskrift != None and utskrift != []:
+    #         utskrifter_placeholder.append(utskrift)
+    #     else:
+    #         utskrifter_placeholder.append("-")
+
+
+    for i, utskrift in enumerate(utskrifter):
+        if i == 0:
+            print(f"\n - {utskrift_beskrivelser[i]}: {utskrift}")
+        elif type(utskrift) == dict:
+            print(f"\t * {utskrift_beskrivelser[i]}: {utskrift['name']}")
+        else:
+            print(f"\t * {utskrift_beskrivelser[i]}: {utskrift}")
