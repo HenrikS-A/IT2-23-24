041434a76785db4fc9de84705596c1ad7b316bdd
HenrikS-A <hstromandersen@gmail.com>
2024-04-18 20:32:19 +0200

    ryddet litt i pactroll-koden

--- 3-prosjekt-og-mappearbeid/uke15/pactroll/pactroll.py
+++ 3-prosjekt-og-mappearbeid/uke15/pactroll/pactroll.py
@@ -7,40 +7,41 @@ class Objekt:
         self.sidelengde = 60
         self.surface = pygame.Surface((self.sidelengde, self.sidelengde))
         self.rect = self.surface.get_rect()
-        
-        # self.rect = pygame.Rect(self.sidelengde, self.sidelengde, self.sidelengde, self.sidelengde)
         self.rect.centerx = x
         self.rect.centery = y
         self.surface.fill(farge)
 
         # Til bokstav tekst:
-        self.fonten = pygame.font.SysFont("Open Sans", 50) # Skrift font
-        self.tekst = self.fonten.render(tekst, True, "black") # Selve teksten
+        self.font = pygame.font.SysFont("Open Sans", 50) # Skrift font
+        self.tekst = self.font.render(tekst, True, "black") # Selve teksten
         self.tekst_ramme = self.tekst.get_rect() # Henter rammen rundt teksten
-        self.tekst_ramme.center = self.rect.center # Plasserer sentrum av tekst-rammen på samme pos. som sentrum til rektangelet.
+        self.tekst_ramme.center = self.rect.center # Plasserer sentrum av tekst-ramme i sentrum til rektangelet
 
     def oppdater(self):
-        self.tekst_ramme.center = self.rect.center
+        self.tekst_ramme.center = self.rect.center 
+        # for hver frame må jeg oppdatere skjermen
+        # slik at tekst og figur hele tiden ligger over hverandre
 
-    def tegn(self, vindu):
-        # pygame.draw.rect(vindu, self.farge, self.rect)
+    def tegn(self, vindu: pygame.Surface):
         vindu.blit(self.surface, self.rect)
-        vindu.blit(self.tekst, (self.tekst_ramme))
+        vindu.blit(self.tekst, self.tekst_ramme)
 
 
 class Troll(Objekt):
     def __init__(self):
-        super().__init__(BREDDE//2, HOYDE//2, "T", "green") # Alle spillere skal ha disse verdiene
+        # I stedet for å velge parametere når jeg oppretter de ulike obketene,
+        # velger jeg her hvilken posisjon, bokstav og farge alle spillere skal ha
+        super().__init__(BREDDE//2, HOYDE//2, "T", "green")
         self.spillerfart = 5 # Konstant fart hele tiden
         self.retning = random.randint(1, 4) # Jeg velger en tilfeldig start-retning for spilleren.
         
     def beveg(self):
 
         # Retninger:
-        # 1 = Høyre
-        # 2 = Ned
-        # 3 = Venstre
-        # 4 = Opp
+        # 1 -> Høyre
+        # 2 -> Ned
+        # 3 -> Venstre
+        # 4 -> Opp
 
         if self.retning == 1:
             self.rect.centerx += self.spillerfart
@@ -50,22 +51,26 @@ class Troll(Objekt):
             self.rect.centerx -= self.spillerfart
         elif self.retning == 4:
             self.rect.centery -= self.spillerfart
+    
+    def sjekk_posisjon(self):
+        if self.rect.left < 0 or self.rect.right > BREDDE or self.rect.top < 0 or self.rect.bottom > HOYDE:
+            pygame.quit()
+            raise SystemExit
+
 
 
 class Matbit(Objekt):
     def __init__(self, x: int, y: int):
-        super().__init__(x, y, "M", "yellow") # Alle matbiter skal ha disse verdiene
+        super().__init__(x, y, "M", "yellow") # Matbiter skal ha bokstav "M" og fargen gul
 
 class Hindring(Objekt):
     def __init__(self, x: int, y: int):
-        super().__init__(x, y, "H", "gray") # Standardverdier
-
+        super().__init__(x, y, "H", "gray") # Hindringer skal ha bokstav "H" og fargen grå
 
 
 
 # 1. Oppsett
 pygame.init()
-
 BREDDE = 1280
 HOYDE = 720
 FPS = 60
@@ -73,28 +78,15 @@ vindu = pygame.display.set_mode((BREDDE, HOYDE))
 klokke = pygame.time.Clock()
 
 pygame.display.set_caption("Pactroll") # Navn på vinduet
-font = pygame.font.SysFont("Open Sans", 24) # Skrifttype
 
-
-
+## Oppsett av spill
 spiller = Troll()
+matbiter = [Matbit(random.randint(30, BREDDE-30), random.randint(30, HOYDE-30)) for _ in range(3)]
 
-
-matbiter = []
-for _ in range(3):
-
-    # Her må jeg legge inn kode for å forsikre meg om at matbitene ikke spawner oppå hverandre.
-
-    matbiter.append(Matbit(random.randint(10, BREDDE-10), random.randint(10, HOYDE-10)))
-
-
-hindringer = []
-
-# For at jeg ikke dør med en gang jeg spiser en matbit
-tillatte_hindringer = []
+hindringer: list[Hindring] = []
+tillatte_hindringer = [] # For at jeg ikke dør med en gang jeg spiser en matbit
 
 kollisjon = False
-
 
 
 while True:
@@ -103,7 +95,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-        
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 spiller.retning = 1
@@ -113,25 +105,25 @@ while True:
                 spiller.retning = 3
             if event.key == pygame.K_UP:
                 spiller.retning = 4
-            
-    
+
     # 3. Oppdater spill
     spiller.beveg()
     spiller.oppdater()
-
+    spiller.sjekk_posisjon()
 
     ## Kollisjoner:
+    
+    # collidelisteall() sjekker hvilke rects som kolliderer og returnerer en liste med indeks-er
     kollisjoner_liste = spiller.rect.collidelistall([matbit.rect for matbit in matbiter])
     if len(kollisjoner_liste) > 0:
-        for i in kollisjoner_liste: # Det er en liste med indeks-er
-
+        for i in kollisjoner_liste: 
             if not kollisjon: 
                 kollisjon = True
 
                 # Spawner en ny matbit på spillebrettet ved en kollisjon
-                matbiter.append(Matbit(random.randint(10, BREDDE-10), random.randint(10, HOYDE-10)))
+                matbiter.append(Matbit(random.randint(30, BREDDE-30), random.randint(30, HOYDE-30)))
 
-                # Jeg legger en hindring rett over matbiten, jeg fjerner ikke matbit.
+                # Jeg legger en hindring rett over matbiten, jeg fjerner ikke matbit
                 ny_hindring = Hindring(matbiter[i].rect.centerx, matbiter[i].rect.centery)
 
                 hindringer.append(ny_hindring)
@@ -146,29 +138,21 @@ while True:
     kollisjonsindeks = spiller.rect.collidelist([hindring.rect for hindring in hindringer])    
     if kollisjonsindeks == -1:
         tillatte_hindringer.clear() # Fjerner alt fra lista!
-        pass
 
 
-    # Hvis spiller kolliderer med hindring (som ikke er tillatt), avslutt spillet
-    for i in range(len(hindringer)):
-        if spiller.rect.colliderect(hindringer[i].rect) and hindringer[i] not in tillatte_hindringer:
+    # Hvis spiller kolliderer med hindring (som ikke er tillatt) -> avslutt spillet
+    for hindring in hindringer:
+        if spiller.rect.colliderect(hindring) and hindring not in tillatte_hindringer:
             pygame.quit()
             raise SystemExit
 
 
     # 4. Tegn
+    vindu.fill("black") # Fyller vinduet med sort bakgrunn for hver gang loopen kjører
 
-    vindu.fill("black") # Fyller vinduet med sort bakgrunn, for hver gang loopen kjører.
-
-    for matbit in matbiter:
-        matbit.tegn(vindu)
-    
-    for hindring in hindringer:
-        hindring.tegn(vindu)
-
-    spiller.tegn(vindu)
-
+    # Slår listene (og spiller objektet) sammen til én liste, så tegner jeg hvert objekt i lista
+    for objekt in [*matbiter, *hindringer, spiller]:
+        objekt.tegn(vindu)
 
     pygame.display.flip() # Oppdaterer skjermen
     klokke.tick(FPS)
-
